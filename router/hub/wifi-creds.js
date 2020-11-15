e8e2aeebd99f37370358c68c03ad1a76e41bc99c
Henrique Santos Azevedo <42189472+HenriqueSantosAzevedo@users.noreply.github.com>
2020-11-16 00:43:25 +0100

    Updated display-settings

--- router/hub/wifi-creds.js
+++ router/hub/wifi-creds.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+//Call shell commands
+const shell = require('shelljs');
+
 const md5 = require('md5');
 const short = require('short-uuid');
 
@@ -8,7 +11,17 @@ router.get('/credentials', async (req, res, next) => {
     const Hostapd = require('../../configuration/hostapd/hostapd.js');
     const wifi = await Hostapd.loadWiFiSettings();
     const code = md5(short().generate())
-    res.render('hub/wifi_creds', {"wifi": {creds: wifi, auth: {code: code, time_until_unusable: 3/*Entries in minutes*/, time_created: Date.now()}}})
+    res.render('hub/wifi_creds', {
+        wifi: {
+            creds: wifi, 
+            auth: {
+                code: code, 
+                time_until_unusable: 3, /*Entries in minutes*/
+                time_created: Date.now()
+            },
+            status: shell.exec('systemctl status hostapd.service', {silent:true}).stdout
+        }
+    })
 })
 
 module.exports.pre_route = "/hub/wifi"
