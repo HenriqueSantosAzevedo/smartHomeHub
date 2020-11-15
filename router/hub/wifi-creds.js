d378a85154d10216ce4a32853df29dd102a84bb6
HenriqueSantosAzevedo <henrique.santos.azevede@gmail.com>
2020-11-15 10:56:05 +0000

    Updated display for self destructive auth-keys

--- router/hub/wifi-creds.js
+++ router/hub/wifi-creds.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-//Implement libraries to create unique code
-//const hash = require('object-hash');
+const md5 = require('md5');
 const short = require('short-uuid');
 
 router.get('/credentials', async (req, res, next) => {
     const Hostapd = require('../../configuration/hostapd/hostapd.js');
     const wifi = await Hostapd.loadWiFiSettings();
-    const code = short().generate()
-    res.render('hub/wifi_creds', {"wifi": {creds: wifi, auth: code}})
+    const code = md5(short().generate())
+    res.render('hub/wifi_creds', {"wifi": {creds: wifi, auth: {code: code, time: 3}}})
 })
 
 module.exports.pre_route = "/hub/wifi"
